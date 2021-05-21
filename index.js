7effde6edd96fdd6c54392ab365a243332482a9e
Calvin Fung <cafung@amazon.com>
2021-05-21 11:04:12 -0700

    Small changes to index.js and added README

--- index.js
+++ index.js
@@ -1,59 +1,21 @@
-// Modifies String object to add reverse method
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
 String.prototype.reverse = function () {
   return Array.from(this).reverse().join("");
 };
 
-// Reverses a string
-// function reverse(string) {
-//     return Array.from(string).reverse().join("");
-// }
-
-// Add a blank method to String object
-String.prototype.blank = function () {
-  return !!this.match(/^\s*$/g);
-};
-
-// Add a 'last' method to the Array object to return last element in an array
-Array.prototype.last = function () {
-  return this[this.length - 1];
-};
-
-// Returns an array of the username and domain for a standard email address
-function emailParts(email) {
-  return email.toLowerCase().split("@");
-}
-
-// Defines a Phrase object
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function (string) {
-    return string.toLowerCase();
-  };
-
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.content.toLowerCase();
   };
 
-  // Returns true for a palindrome, false otherwise.
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
-  };
-
-  // Makes the phrase LOUDER
-  this.louder = function louder() {
-    return this.content.toUpperCase();
-  };
-}
-
-// Defines a TranslatedPhrase object;
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
   };
 }
